8fab8c17875de53c61bc30d1a6a0e5e969eb7f9f
miguelnkono <nkonondememiguel7@gmail.com>
2026-01-12 15:09:40 +0100

    fix the const deferencement in the find_if algorithm

--- register.cpp
+++ register.cpp
@@ -12,7 +12,7 @@ uint64_t get_register_value(pid_t pid, reg r) {
   // we search for the index of the register requested in the regs structure.
   const reg_descriptor *it =
       std::find_if(begin(g_register_descriptors), end(g_register_descriptors),
-                   [r](reg_descriptor &&rd) { return rd.r == r; });
+                   [r](const reg_descriptor &rd) { return rd.r == r; });
 
   return *(reinterpret_cast<uint64_t *>(&regs) +
            (it - begin(g_register_descriptors)));
@@ -25,7 +25,7 @@ void set_register_value(pid_t pid, reg r, uint64_t value) {
   // we search for the index of the register requested in the regs structure.
   const reg_descriptor *it =
       std::find_if(begin(g_register_descriptors), end(g_register_descriptors),
-                   [r](reg_descriptor &&rd) { return rd.r == r; });
+                   [r](const reg_descriptor &rd) { return rd.r == r; });
 
   *(reinterpret_cast<uint64_t *>(&regs) +
     (it - begin(g_register_descriptors))) = value;
@@ -34,9 +34,11 @@ void set_register_value(pid_t pid, reg r, uint64_t value) {
 
 uint64_t get_register_value_from_dwart_register(pid_t pid, unsigned regnum) {
 
-  const reg_descriptor *it = std::find_if(
-      begin(g_register_descriptors), end(g_register_descriptors),
-      [regnum](reg_descriptor &&rd) -> bool { return rd.dwarf_r == regnum; });
+  const reg_descriptor *it =
+      std::find_if(begin(g_register_descriptors), end(g_register_descriptors),
+                   [regnum](const reg_descriptor &rd) -> bool {
+                     return rd.dwarf_r == regnum;
+                   });
 
   // if we didn't find the register by its dwarf (find_if returned the last the
   // element of g_register_descriptors)
@@ -50,14 +52,14 @@ uint64_t get_register_value_from_dwart_register(pid_t pid, unsigned regnum) {
 std::string get_register_name(reg r) {
   const reg_descriptor *it =
       std::find_if(begin(g_register_descriptors), end(g_register_descriptors),
-                   [r](reg_descriptor &&rd) -> bool { return rd.r == r; });
+                   [r](const reg_descriptor &rd) -> bool { return rd.r == r; });
   return it->name;
 }
 
 reg get_register_from_name(const std::string &name) {
   const reg_descriptor *it = std::find_if(
       begin(g_register_descriptors), end(g_register_descriptors),
-      [name](reg_descriptor &&rd) -> bool { return rd.name == name; });
+      [name](const reg_descriptor &rd) -> bool { return rd.name == name; });
 
   return it->r;
 }
